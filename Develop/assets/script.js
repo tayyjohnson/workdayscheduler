4792801aa5d0cfa2782f295bf58102c5a784afcc
tayyjohnson <taydriver1@gmail.com>
2022-06-29 15:20:00 -0400

    reorganized code and fixed issue with times now showing up

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -3,13 +3,23 @@ document.getElementById("currentDay") .innerHTML=moment().format('LL');
 
 //color coding for upcoming dates
 var timeColor=function() {
+    //current hour
     var hour=parseInt(moment().format('HH'));
-    
-    //apply new class if task is in the past/present/future
-    if(moment().isAfter(hour)) {
-        $(descriptionE1).addclass("description-item-warning")
-    }
+
+    $(".time-block").each(function() {
+        var dataHour=parseInt($(this).attr("id"));
+        if (dataHour<hour) {
+        }else if (dataHour===hour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
 };
+
 
 //save button
 $('.saveBtn').click(function() {
@@ -28,11 +38,11 @@ $("#9".description).val(localStorage.getItem(9));
 $("#10".description).val(localStorage.getItem(10));
 $("#11".description).val(localStorage.getItem(11));
 $("#12".description).val(localStorage.getItem(12));
-$("#1".description).val(localStorage.getItem(1));
-$("#2".description).val(localStorage.getItem(2));
-$("#3".description).val(localStorage.getItem(3));
-$("#4".description).val(localStorage.getItem(4));
-$("#5".description).val(localStorage.getItem(5));
+$("#13".description).val(localStorage.getItem(13));
+$("#14".description).val(localStorage.getItem(14));
+$("#15".description).val(localStorage.getItem(15));
+$("#16".description).val(localStorage.getItem(16));
+$("#17".description).val(localStorage.getItem(17));
 
 
 timeColor();
